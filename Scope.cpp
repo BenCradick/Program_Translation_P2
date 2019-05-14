ffb4cde99b9c7b2adaf6f053a243144c34a5cf0e
BenCradick <ben.cradick@gmail.com>
2019-05-14 15:11:46 -0500

    debugged P3 on macbook, works with clang

--- Scope.cpp
+++ Scope.cpp
@@ -5,17 +5,30 @@
 #include "Scope.h"
 
 #include <algorithm>
+#include <string>
+#include <stdexcept>
+#include <iostream>
 
 
 void Scope::setGlobal(Node* root) {
+    if (root == nullptr){
+        return;
+    }
 
     if(root->token_count == 2)
     {
+        if(root->token[1] == nullptr){
+            for(int i = 0; i < root->child_count; i++)
+            {
+                setGlobal(root->children[i]);
+            }
+            return;
+        }
         if(global.find(root->token[0]->instance) == global.end() && root->token[0]->t_type == id_tk)
         {
             if(root->token[1]->t_type == int_tk)
             {
-                global.insert(root->token[0]->instance, root->token[1]->instance);
+                global.insert({root->token[0]->instance, root->token[1]->instance});
             }
             else
             {
@@ -30,11 +43,11 @@ void Scope::setGlobal(Node* root) {
                 exit(EXIT_FAILURE);
             }
         }
-        else
-        {
-            std::fprintf(stderr, "var %s is already defined in the global scope\n", root->token[0]->instance.c_str());
-            exit(EXIT_FAILURE);
-        }
+//        else
+//        {
+//            std::fprintf(stderr, "var %s is already defined in the global scope\n", root->token[0]->instance.c_str());
+//            exit(EXIT_FAILURE);
+//        }
     }
     else if(root->token_count == 1)
     {
--- Scope.h
+++ Scope.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <iostream>
+#include <string>
 #include <map>
 #include "Node.h"
 
